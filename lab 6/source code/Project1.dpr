8c410e943e8bac2b1ac4b81c66db23ec369e2e49
Sergey <33185046+OneManDevelop@users.noreply.github.com>
2019-10-09 10:30:55 +0400

    lab 6
    report in dev

--- lab 6/source code/Project1.dpr
+++ lab 6/source code/Project1.dpr
@@ -9,7 +9,7 @@ type
   gl = set of char;
 
 var
-  a: string;
+  a, s: string;
   glas: gl;
   i, k: byte;
   c1, c2: char;
@@ -19,27 +19,41 @@ begin
   glas := ['a', 'e', 'i', 'o', 'u'];
   writeln('Enter string');
   readln(a);
-  a := LowerCase(a);
+  a := LowerCase(a); { registr independence }
   k := 0;
+  c1 := '.'; { if c1 or c2 won't be set in program }
+  c2 := '.';
 
   i := 1;
+  s := '';
+
+  while (pos(' ', a) <> 0) do
+    delete(a, pos(' ', a), 1);
+
+  write('LowerCased zadannie stroki: ');
+
   fl := true; { new word beginning }
-  while (a[i] <> '.') do
+  while ((a[i] <> '.') and (i <= length(a))) do
   begin
-    if ((a[i] <> '.') and (a[i] <> ',')) then
+    if (a[i] <> ',') then
     begin
       if fl then
       begin
+        s := '';
         c1 := a[i];
         fl := false;
       end
       else
         c2 := a[i];
+      s := s + a[i];
     end
     else
     begin
       if ((c1 in glas) and (c2 in glas)) then
+      begin
+        write(s, ' ');
         k := k + 1;
+      end;
       fl := true;
     end;
     i := i + 1;
@@ -47,7 +61,12 @@ begin
   end;
 
   if ((c1 in glas) and (c2 in glas)) then
+  begin
+    write(s, ' ');
     k := k + 1;
+  end;
+
+  writeln;
 
   writeln('Zadannih slov: ', k);
   readln;
