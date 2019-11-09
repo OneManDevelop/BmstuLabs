9f58af4b9e4231f6ac37d125fe4e4a0b39deb074
ASV <fzastahov@gmail.com>
2019-11-09 23:59:46 +0300

    lab 9 issued

---
+++ lab 9/src/Project2.dpr
@@ -0,0 +1,83 @@
+program Project2;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils;
+
+type
+  UchPointer = ^uchastnik;
+
+  uchastnik = record
+    fam: string;
+    nxt: UchPointer;
+  end;
+
+var
+  p1, p2, pst, pfn: UchPointer;
+  uch1: uchastnik;
+  a: string;
+
+procedure SortList(pf: UchPointer);
+var
+  ptarget, buffer: UchPointer;
+  sorted, swapped: boolean;
+begin
+  { ptarget:=pf; }
+  sorted := false;
+  while not sorted do
+  begin
+    sorted := true;
+    ptarget := pf;
+    while (ptarget^.nxt <> nil) do
+    begin
+      swapped := false;
+      if (ptarget^.fam > ptarget^.nxt^.fam) then
+      begin
+        sorted := false;
+        buffer := ptarget^.nxt;
+        ptarget^.nxt := ptarget^.nxt^.nxt;
+        ptarget^.nxt^.nxt := buffer;
+        swapped := true;
+      end;
+      if not swapped then
+        ptarget := ptarget^.nxt;
+      { depends from swapping maybe }
+    end;
+
+  end;
+
+end;
+
+begin
+  writeln('Enter surnames');
+  readln(a);
+  new(pst);
+  pst^.fam := a;
+  p1 := pst;
+  readln(a);
+  while (a <> '') do
+  begin
+    new(p2);
+    p2^.fam := a;
+    p1^.nxt := p2;
+    p1 := p2;
+    readln(a);
+  end;
+  p1^.nxt := nil;
+  p2^.nxt := nil;
+  pfn := p1;
+
+  SortList(pst);
+
+  p1 := pst;
+  while (p1^.nxt <> nil) do
+  begin
+    writeln(p1^.fam);
+    p1 := p1^.nxt;
+  end;
+  writeln(p1^.fam);
+
+  readln;
+
+end.
